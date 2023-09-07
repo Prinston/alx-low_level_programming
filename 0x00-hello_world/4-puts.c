d5b4e8ae7cb9f20421a482fe404bc04f51035046
Prinston <Prinstonsarfo@gmail.com>
2023-09-07 15:44:01 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this is a print function
+ * Return: 0
+ */
+
+int man(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+
+
+}
